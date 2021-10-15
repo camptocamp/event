4b00c1ea8582203991177b9cbb16ab148bc2a8bc
Damien Crier <damien.crier@camptocamp.com>
2021-10-15 11:12:59 +0200

    [ADD] module website_event_sale_hide_ticket

---
+++ website_event_sale_hide_ticket/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2020 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+{
+    "name": "Website Event Sale Hide Ticket",
+    "summary": "Allow to hide event ticket from the website",
+    "version": "13.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Website/Website",
+    "website": "https://github.com/OCA/event",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["website_event_sale", "website_event_sale_single_attendee"],
+    "data": ["views/event_ticket.xml", "templates/event_ticket.xml"],
+}

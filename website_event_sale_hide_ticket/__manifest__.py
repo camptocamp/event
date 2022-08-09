f5f8afb6fa3afe0325ef96778295724aced35b05
Camille Morand <camille.morand@camptocamp.com>
2022-08-09 16:22:07 +0200

    [MIG] website_event_sale_hide_ticket: Migration to 15.0

--- website_event_sale_hide_ticket/__manifest__.py
+++ website_event_sale_hide_ticket/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Website Event Sale Hide Ticket",
     "summary": "Allow to hide event ticket from the website",
-    "version": "13.0.1.0.0",
+    "version": "15.0.1.0.0",
     "development_status": "Beta",
     "category": "Website/Website",
     "website": "https://github.com/OCA/event",
